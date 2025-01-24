c89f765d576a26d7cd1ebcadb6f5c0fca246d8b7
Emir Çavdar <emircvdar@gmail.com>
2025-01-24 20:01:15 +0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Statik export için gerekli ayar
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", // Statik export için gerekli ayar
-// };
-
-// export default nextConfig;
